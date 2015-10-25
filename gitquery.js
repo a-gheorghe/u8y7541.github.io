e30339069a1d5e865116c98bf1228a1390dc1fb6
u8y7541 <surya.subbarao1@gmail.com>
2015-10-25 09:27:51 -0700

    Github querier
    This file queries GitHub and gets my repos. Then it puts them on the
    sidebar.

---
+++ gitquery.js
@@ -0,0 +1,15 @@
+var req = new XMLHttpRequest();
+req.open('GET', 'https://api.github.com/users/u8y7541/repos', false);
+req.send(null);
+if(req.status == 200)
+  a = req.responseText;
+a = JSON.parse(a);
+b = [];
+for (i in a) {
+	b.push(a[i]['name']);
+}
+result = '';
+for (i in b) {
+	result += '<li><a href = https://github.com/u8y7541/' + b[i] + '>' + b[i] + '</a></li>';
+}
+document.getElementById('git').innerHTML = result;
